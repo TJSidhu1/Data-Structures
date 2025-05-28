340edca37567e100ad918c804130d4f5df4ed6c1
TJSidhu1 <tsidhu@csuchico.edu>
2025-05-27 17:41:43 -0700

    Additional methods to find head and tail of a linked list

--- LinkedLists/LinkedList.cpp
+++ LinkedLists/LinkedList.cpp
@@ -15,6 +15,11 @@ class Node
 
 };
 
+void print_head(Node* head)
+{
+    cout << "Head Node:" << head->value << endl;
+}
+
 void display(Node *head)
     {
 
@@ -23,11 +28,35 @@ void display(Node *head)
             cout << head->value << " " ;
             head = head->next;
         }
+        cout << endl;
     }
+
+void printTail(Node* head)
+{
+    if(head == nullptr)
+    {
+        cout << "Linked List is empty" << endl;
+    }
+
+    else
+    {
+        while(head->next != nullptr)
+        {
+            head = head->next;
+        }
+       cout << "Tail node: " << head->value << endl; 
+    }
+}    
 
 int main()
 {
     Node *head = new Node(1);
+    Node *l1 = new Node(2);
+    Node *l2 = new Node(3);
+    head->next = l1;
+    l1->next = l2;
     display(head);
+    print_head(head);
+    printTail(head);
     return 0;
 }
