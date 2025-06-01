d7aa7fa9a23326f882cc2f2b8793b2a18ac7ffed
TJSidhu1 <tsidhu@csuchico.edu>
2025-05-31 23:12:47 -0700

    Adding Tree structure

--- Trees/BinaryTree.cpp
+++ Trees/BinaryTree.cpp
@@ -2,11 +2,24 @@ class Node{
    
     public:
            int value;
-           Node* next;
+           Node* left;
+           Node* right;
 
            Node(int val)
            {
              value = val;
-             next = nullptr;
+             left = nullptr;
+             right = nullptr;
            }
 };
+
+int main()
+{
+    Node *root = new Node(1);
+    Node *l1 = new Node(2);
+    Node *l2 = new Node(3);
+    root->left = l1;
+    root->right = l2;
+
+    return 0;
+}
