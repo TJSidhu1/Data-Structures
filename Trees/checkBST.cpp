6239e9a6468028630d61c243b44d254535740766
TJSidhu1 <tsidhu@csuchico.edu>
2025-06-02 23:47:11 -0700

    Adding final code modification to checkBST and testing it with the tree creation

--- Trees/checkBST.cpp
+++ Trees/checkBST.cpp
@@ -1,4 +1,6 @@
 #include <climits>
+#include <iostream>
+using namespace std;
 
 class Node{
 
@@ -13,13 +15,6 @@ class Node{
     }
 };
 
-bool isBST(Node* root)
-{
-    int min = INT_MIN;
-    int max = INT_MAX;
-    return helper(root, min, max);
-}
-
 bool helper(Node* root, int min, int max)
 {
     if(root == nullptr)
@@ -32,4 +27,32 @@ bool helper(Node* root, int min, int max)
 
     else
         return false;
+}
+
+
+bool isBST(Node* root)
+{
+    int min = INT_MIN;
+    int max = INT_MAX;
+    return helper(root, min, max);
+}
+
+
+
+int main()
+{
+    Node* root = new Node(5);
+    Node* left1 = new Node(3);
+    Node* right1 = new Node(7);
+    Node* right2 = new Node(4);
+    Node* left2 = new Node(6);
+
+    root->left = left1;
+    root->right = right1;
+    right1->left = left2;
+    left1->right = right2;
+
+    cout << "The given tree is Binary search tree (Yes : 1/ No : 0) : "<< isBST(root) << endl;
+
+
 }
